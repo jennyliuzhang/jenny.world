34bcee48e6e895fa61229ef876b1f57fc71af111
Jenny Zhang <jennylzh@usc.edu>
2018-02-20 14:58:33 -0800

    update again ux

--- ux/js.js
+++ ux/js.js
@@ -1,3 +1,15 @@
+$(document).ready(function() {
+  // if (getCookie("youveBeenHere") == "yep") {
+  //   console.log("You've been here before!");
+  // }
+  // else {
+  //   $('#loading').css("display", "block");
+  //   $('#loading').delay(8000).fadeOut(1000);
+  //   $('#gif').delay(7000).fadeOut(1000);
+  //   Cookies.set("youveBeenHere", "yep");
+  // }
+});
+
 function toggleProject (event,project) {
   event.preventDefault();
   event.stopPropagation();
